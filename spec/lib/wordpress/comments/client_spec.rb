c12569aed1b3658fcc66efb09656bab0d2b2ee55
SaraLerma <sara.lerma@coverwallet.com>
2020-05-21 12:45:18 +0200

    Refactor parser (#5)
    * refactor client
    
    * unicode

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -1,34 +1,14 @@
-#require 'jquery-rails'
-require 'nokogiri'
-
-module Wordpress
-    module Comments
-        class Client
-
-            attr_reader :url
-
-            def initialize url
-                @url = url
-            end
-            def parse xml
-                doc = Nokogiri::XML xml
-                doc.search('item').map do |doc_item|
-                    item = {}
-                    item[:link] = doc_item.at('link').text
-                    item
-                end
-            end
-        end
-    end
-end
+#coding: utf-8
+require_relative '../../../../lib/wordpress/comments/client'
 
 describe Wordpress::Comments::Client do
+
+    let(:client) {Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'}
 
     describe "#initialize" do
 
         it 'store a URL' do
             #create a Client instace with the argument 'http://mashable.com/comments/feed'
-            client = Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'
             expect(client.url).to eq 'http://mashable.com/comments/feed'
         end
 
@@ -37,13 +17,17 @@ describe Wordpress::Comments::Client do
     describe "#parse" do
 
         let(:xml) {File.read(File.join('spec', 'fixtures', 'feed.xml'))}
+        
+        let(:comments) {client.parse xml}
+        let(:comment) {comments.first}
 
         it 'extracts the link' do
-            client = Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'
             link= 'http://mashable.com/2012/07/18/ipad-early-photos/comment-page-1/#comment-18239503'
-            comments = client.parse xml
-            comment = comments.first
             expect(comment[:link]).to eq link
+        end
+        it 'extracts the title' do
+            title= "Comment on The Earliest Photos of Apple’s iPad Hit the Web by Fido"
+            expect(comment[:title]).to eq title
         end
 
     end
